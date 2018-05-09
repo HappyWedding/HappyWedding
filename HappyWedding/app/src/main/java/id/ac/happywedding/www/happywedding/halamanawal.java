8d72ec09b301c0afd517cf262fa96b26cf4e2a24
lidiyasuryaningsih22 <lidiyasuryaningsih22@gmail.com>
2018-05-09 06:30:05 -1200

    desain xml di android

--- HappyWedding/app/src/main/java/id/ac/happywedding/www/happywedding/halamanawal.java
+++ HappyWedding/app/src/main/java/id/ac/happywedding/www/happywedding/halamanawal.java
@@ -1,7 +1,11 @@
 package id.ac.happywedding.www.happywedding;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class halamanawal extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class halamanawal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halamanawal);
+
+        Button button = (Button) findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+
+            public void onClick(View arg0) {
+
+                // TODO Auto-generated method stub
+
+                Intent i = new Intent(getApplicationContext(), login.class);
+
+                startActivity(i);
+
+
+            }
+        });
+
     }
 }
